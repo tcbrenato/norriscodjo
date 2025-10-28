1c51223ec59203a24aec9930e972177cefa90a61
TCHOBO Y. Rénato <167759544+tcbrenato@users.noreply.github.com>
2025-10-28 15:50:35 +0100

    Updated albumsData.ts

--- src/components/albumsData.ts
+++ src/components/albumsData.ts
@@ -1,38 +1,36 @@
-export const albumsData = [
+export const albums = [
   {
-    id: 1,
     name: "Affiches",
     cover: "https://i.ibb.co/cover-affiche.jpg",
-    images: []
+    images: [
+      "https://i.ibb.co/photo1.jpg",
+      "https://i.ibb.co/photo2.jpg",
+      "https://i.ibb.co/photo3.jpg"
+    ]
   },
   {
-    id: 2,
     name: "Enseignes",
     cover: "https://i.ibb.co/cover-enseigne.jpg",
     images: []
   },
   {
-    id: 3,
     name: "Stands",
     cover: "https://i.ibb.co/cover-stand.jpg",
     images: []
   },
   {
-    id: 4,
     name: "Décorations",
     cover: "https://i.ibb.co/cover-decoration.jpg",
     images: []
   },
   {
-    id: 5,
     name: "Bâches",
     cover: "https://i.ibb.co/cover-bache.jpg",
     images: []
   },
   {
-    id: 6,
-    name: "Autres Réalisations",
-    cover: "https://i.ibb.co/cover-autres.jpg",
+    name: "Autres réalisations",
+    cover: "https://i.ibb.co/cover-autre.jpg",
     images: []
   }
 ];
