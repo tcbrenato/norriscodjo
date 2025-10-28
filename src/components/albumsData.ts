82420fb4bfec8f92e7098dd65d8448fff43c3178
TCHOBO Y. Rénato <167759544+tcbrenato@users.noreply.github.com>
2025-10-28 13:39:57 +0100

    Updated albumsData.ts

---
+++ src/components/albumsData.ts
@@ -0,0 +1,38 @@
+export const albumsData = [
+  {
+    id: 1,
+    name: "Affiches",
+    cover: "https://i.ibb.co/cover-affiche.jpg",
+    images: []
+  },
+  {
+    id: 2,
+    name: "Enseignes",
+    cover: "https://i.ibb.co/cover-enseigne.jpg",
+    images: []
+  },
+  {
+    id: 3,
+    name: "Stands",
+    cover: "https://i.ibb.co/cover-stand.jpg",
+    images: []
+  },
+  {
+    id: 4,
+    name: "Décorations",
+    cover: "https://i.ibb.co/cover-decoration.jpg",
+    images: []
+  },
+  {
+    id: 5,
+    name: "Bâches",
+    cover: "https://i.ibb.co/cover-bache.jpg",
+    images: []
+  },
+  {
+    id: 6,
+    name: "Autres Réalisations",
+    cover: "https://i.ibb.co/cover-autres.jpg",
+    images: []
+  }
+];
